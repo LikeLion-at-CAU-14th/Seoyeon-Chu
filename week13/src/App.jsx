107d8225b57485c0a230e1426830046375d675ae
cntjdus <smonstacy@cau.ac.kr>
2026-07-04 22:15:42 +0900

    13주차 과제 (1)

--- week13/src/App.jsx
+++ week13/src/App.jsx
@@ -4,15 +4,17 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MyPage from './pages/MyPage';
 import Layout from './components/layout/Layout';
-
+import { UserInfoProvider } from './context/UserInfoContext';
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <UserInfoProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </UserInfoProvider>
     </Layout>
   )
 }
